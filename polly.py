12a34124f681894dd30e65756128a92c28eb2143
John Alex Hebert <3825230+johnalexhebert@users.noreply.github.com>
2020-09-21 20:01:33 -0500

    Update polly.py
    Added comment

--- polly.py
+++ polly.py
@@ -7,6 +7,7 @@ def dateDiffInDays(date1, date2):
   timedelta = date2 - date1
   return timedelta.days
 
+# test date
 election_date = datetime.strptime('2020-11-03', '%Y-%m-%d')
 now = datetime.now()
 countdown = dateDiffInDays(now, election_date) + 1
